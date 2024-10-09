9fb5ba81c1bba04ce4e81a2a5e722696b96db4df
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-10-09 09:28:02 +0700

    commit for hosting

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyB2Ei4adzbkRwE11_hveyKIoRJHitedIq4',
-  authDomain: 'twitter-app-d54f2.firebaseapp.com',
-  projectId: 'twitter-app-d54f2',
-  storageBucket: 'twitter-app-d54f2.appspot.com',
-  messagingSenderId: '573897192586',
-  appId: '1:573897192586:web:9baf46009b8ea5fd56e973',
-  measurementId: 'G-3XB6GL3R51',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
